791e02762307124324df02ea5161929c758ca9a0
Wally <1161512420@qq.com>
2022-02-19 00:20:45 +0800

    u

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">宁德时代</a></li>
             <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">通策医疗</a></li>
     </ul>
-    <h3>未来一周大盘预测</h3>
+    <h3>未来五日大盘预测</h3>
     <div ref="chart" style="width:100%;height:376px"></div>
     <h3>未来一周行业预测</h3>
     <ul>
@@ -41,28 +41,110 @@ export default {
       return [start, end]
     },
     getEchartData(index_quote) {
+      let x_data = index_quote.data["000001.SH"].date
+      let date_num = x_data.length
+      console.log(date_num)
       const chart = this.$refs.chart
       
       if (chart) {
         const myChart = this.$echarts.init(chart)
         const option = {
-                xAxis: {
-                  type: 'category',
-                  data: index_quote.data["000001.SH"].date
-                },
-                yAxis: {
-                  type: 'value',
-                  min: function(value) {return value.min - 100;},
-                  max: function(value) {return value.max + 100;},
-                },
-                series: [
-                  {
-                    data: index_quote.data["000001.SH"].value,
-                    type: 'line',
-                    smooth: true
-                  }
-                ]
-              };
+  title: {
+    text: '上证指数',
+    subtext: '最新趋势图'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross'
+    }
+  },
+  toolbox: {
+    show: true,
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    // prettier-ignore
+    data: index_quote.data["000001.SH"].date
+  },
+  yAxis: {
+    type: 'value',
+    min: function(value) {return value.min - 100;},
+    max: function(value) {return value.max + 100;},
+    axisLabel: {
+      formatter: '{value} '
+    },
+    axisPointer: {
+      snap: true
+    }
+  },
+  visualMap: {
+    show: false,
+    dimension: 0,
+    pieces: [
+      {
+        lte: date_num - 6,
+        color: 'green'
+      },
+      // {
+      //   gt: 100,
+      //   color: 'red'
+      // },
+      {
+        gt: date_num - 6,
+        lte: 10000,
+        color: 'rgba(30, 144, 255, 0.9)'
+      },
+      // {
+      //   gt: 50,
+      //   lte: 53,
+      //   color: 'red'
+      // },
+      // {
+      //   gt: 53,
+      //   color: 'green'
+      // }
+    ]
+  },
+  series: [
+    {
+      name: '收盘价',
+      type: 'line',
+      smooth: true,
+      // prettier-ignore
+      data: index_quote.data["000001.SH"].value,
+      markArea: {
+        itemStyle: {
+          color: 'rgba(135, 206, 235, 0.4)'
+        },
+        data: [
+          [
+            {
+              name: '预测区间',
+              xAxis: x_data[date_num-6]
+            },
+            {
+              xAxis: x_data[date_num-1]
+            }
+          ],
+          // [
+          //   {
+          //     name: 'Evening Peak',
+          //     xAxis: '17:30'
+          //   },
+          //   {
+          //     xAxis: '21:15'
+          //   }
+          // ]
+        ]
+      }
+    }
+  ]
+};
         myChart.setOption(option)
         window.addEventListener("resize", function() {
           myChart.resize()
@@ -73,6 +155,39 @@ export default {
           myChart.resize();
         });
         })
+      },
+      getEchartData2(index_quote) {
+        const chart = this.$refs.chart
+        if (chart) {
+          const myChart = this.$echarts.init(chart)
+          const option = {
+                  xAxis: {
+                    type: 'category',
+                    data: index_quote.data["000001.SH"].date
+                  },
+                  yAxis: {
+                    type: 'value',
+                    min: function(value) {return value.min - 100;},
+                    max: function(value) {return value.max + 100;},
+                  },
+                  series: [
+                    {
+                      data: index_quote.data["000001.SH"].value,
+                      type: 'line',
+                      smooth: true
+                    }
+                  ]
+                };
+          myChart.setOption(option)
+          window.addEventListener("resize", function() {
+            myChart.resize()
+          })
+        }
+        this.$on('hook:destroyed',()=>{
+            window.removeEventListener("resize", function() {
+            myChart.resize();
+          });
+          })
       }
   }
 }
