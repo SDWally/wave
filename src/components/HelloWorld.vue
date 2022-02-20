6e00cd01b30cc33e08a19abe2e72e115b13af65c
Wally <1161512420@qq.com>
2022-02-20 13:07:31 +0800

    u

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,10 +9,9 @@
     </ul>
     <h3>未来五日大盘预测</h3>
     <div ref="chart" style="width:100%;height:376px"></div>
-    <h3>未来一周行业预测</h3>
-    <div v-for="item in industry" :key="item" :ref="item" class="flex-item"></div>
+    <h3>未来五日行业预测</h3>
     <div class="flex-container">
-
+          <div v-for="item in industry" :key="item" :ref="item" class="flex-item"></div>
     </div>
   </div>
 </template>
@@ -24,7 +23,19 @@ export default {
   name: 'HelloWorld',
   data() {
     return { 
-      industry: ["bank", "car"] 
+      industry: [{
+        "name": "bank",
+        "date": [],
+        "value": []
+      }, {
+        "name": "car",
+        "date": [],
+        "value": []
+      }, {
+        "name": "infrastructure",
+        "date": [],
+        "value": []
+      }]
       }
   },
   props: {
@@ -32,10 +43,9 @@ export default {
   },
   mounted(){
     let dates = this.get_start_and_end();
-    let start = dates[0]
-    let end = dates[1]
-    getFactor("factor_index_quote_close", "000001.SH", start, end).then(this.getEchartData);
-    getFactor("factor_index_quote_close", "000001.SH", start, end).then(this.getEchartIndustry);
+    let dates_industry = this.get_start_and_end(90)
+    getFactor("factor_index_quote_close", "000001.SH", dates[0], dates[1]).then(this.getEchartData);
+    getFactor("factor_index_quote_close", "000001.SH", dates_industry[0], dates_industry[1]).then(this.getEchartIndustry);
   },
   methods: {
     get_start_and_end(days=180) {
@@ -46,107 +56,85 @@ export default {
     getEchartData(index_quote) {
       let x_data = index_quote.data["000001.SH"].date
       let date_num = x_data.length
-      console.log(date_num)
+      // console.log(date_num)
       const chart = this.$refs.chart
       
       if (chart) {
         const myChart = this.$echarts.init(chart)
         const option = {
-  title: {
-    text: '上证指数',
-    subtext: '最新趋势图'
-  },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross'
-    }
-  },
-  toolbox: {
-    show: true,
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
-    // prettier-ignore
-    data: index_quote.data["000001.SH"].date
-  },
-  yAxis: {
-    type: 'value',
-    min: function(value) {return value.min - 100;},
-    max: function(value) {return value.max + 100;},
-    axisLabel: {
-      formatter: '{value} '
-    },
-    axisPointer: {
-      snap: true
-    }
-  },
-  visualMap: {
-    show: false,
-    dimension: 0,
-    pieces: [
-      {
-        lte: date_num - 6,
-        color: 'green'
-      },
-      // {
-      //   gt: 100,
-      //   color: 'red'
-      // },
-      {
-        gt: date_num - 6,
-        lte: 10000,
-        color: 'rgba(30, 144, 255, 0.9)'
-      },
-      // {
-      //   gt: 50,
-      //   lte: 53,
-      //   color: 'red'
-      // },
-      // {
-      //   gt: 53,
-      //   color: 'green'
-      // }
-    ]
-  },
-  series: [
-    {
-      name: '收盘价',
-      type: 'line',
-      smooth: true,
-      // prettier-ignore
-      data: index_quote.data["000001.SH"].value,
-      markArea: {
-        itemStyle: {
-          color: 'rgba(135, 206, 235, 0.4)'
-        },
-        data: [
-          [
-            {
-              name: '预测区间',
-              xAxis: x_data[date_num-6]
-            },
-            {
-              xAxis: x_data[date_num-1]
+          title: {
+            text: '上证指数',
+            subtext: '最新趋势图'
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross'
             }
-          ],
-          // [
-          //   {
-          //     name: 'Evening Peak',
-          //     xAxis: '17:30'
-          //   },
-          //   {
-          //     xAxis: '21:15'
-          //   }
-          // ]
-        ]
-      }
-    }
-  ]
+          },
+          toolbox: {
+            show: true,
+            feature: {
+              saveAsImage: {}
+            }
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            // prettier-ignore
+            data: index_quote.data["000001.SH"].date
+          },
+          yAxis: {
+            type: 'value',
+            min: function(value) {return value.min - 100;},
+            max: function(value) {return value.max + 100;},
+            axisLabel: {
+              formatter: '{value} '
+            },
+            axisPointer: {
+              snap: true
+            }
+          },
+          visualMap: {
+            show: false,
+            dimension: 0,
+            pieces: [
+              {
+                lte: date_num - 6,
+                color: 'green'
+              },
+              {
+                gt: date_num - 6,
+                lte: 10000,
+                color: 'rgba(30, 144, 255, 0.9)'
+              },
+            ]
+          },
+          series: [
+            {
+              name: '收盘价',
+              type: 'line',
+              smooth: true,
+              // prettier-ignore
+              data: index_quote.data["000001.SH"].value,
+              markArea: {
+                itemStyle: {
+                  color: 'rgba(135, 206, 235, 0.4)'
+                },
+                data: [
+                  [
+                    {
+                      name: '预测区间',
+                      xAxis: x_data[date_num-6]
+                    },
+                    {
+                      xAxis: x_data[date_num-1]
+                    }
+                  ],
+                ]
+              }
+            }
+          ]
 };
         myChart.setOption(option)
         window.addEventListener("resize", function() {
@@ -160,29 +148,88 @@ export default {
         })
       },
       getEchartIndustry(index_quote) {
+        let x_data = index_quote.data["000001.SH"].date
+        let date_num = x_data.length
         for (let ind in this.industry) {
-        const chart = this.$refs[this.industry[ind]]
+        const chart = this.$refs[this.industry[ind]][0]
         console.log(chart)
         if (chart) {
           const myChart = this.$echarts.init(chart)
           const option = {
-                  xAxis: {
-                    type: 'category',
-                    data: index_quote.data["000001.SH"].date
+            title: {
+              text: '上证指数',
+              subtext: '最新趋势图'
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                type: 'cross'
+              }
+            },
+            toolbox: {
+              show: true,
+              feature: {
+                saveAsImage: {}
+              }
+            },
+            xAxis: {
+              type: 'category',
+              boundaryGap: false,
+              // prettier-ignore
+              data: index_quote.data["000001.SH"].date
+            },
+            yAxis: {
+              type: 'value',
+              min: function(value) {return value.min - 100;},
+              max: function(value) {return value.max + 100;},
+              axisLabel: {
+                formatter: '{value} '
+              },
+              axisPointer: {
+                snap: true
+              }
+            },
+            visualMap: {
+              show: false,
+              dimension: 0,
+              pieces: [
+                {
+                  lte: date_num - 6,
+                  color: 'green'
+                },
+                {
+                  gt: date_num - 6,
+                  lte: 10000,
+                  color: 'rgba(30, 144, 255, 0.9)'
+                },
+              ]
+            },
+            series: [
+              {
+                name: '收盘价',
+                type: 'line',
+                smooth: true,
+                // prettier-ignore
+                data: index_quote.data["000001.SH"].value,
+                markArea: {
+                  itemStyle: {
+                    color: 'rgba(135, 206, 235, 0.4)'
                   },
-                  yAxis: {
-                    type: 'value',
-                    min: function(value) {return value.min - 100;},
-                    max: function(value) {return value.max + 100;},
-                  },
-                  series: [
-                    {
-                      data: index_quote.data["000001.SH"].value,
-                      type: 'line',
-                      smooth: true
-                    }
+                  data: [
+                    [
+                      {
+                        name: '预测区间',
+                        xAxis: x_data[date_num-6]
+                      },
+                      {
+                        xAxis: x_data[date_num-1]
+                      }
+                    ],
                   ]
-                };
+                }
+              }
+            ]
+  };
           myChart.setOption(option)
           window.addEventListener("resize", function() {
             myChart.resize()
@@ -193,8 +240,7 @@ export default {
             myChart.resize();
           });
           })
-      }
-    }
+    }}
   }
 }
 </script>
