40db2d87a4e0bd9ba7be03a648e51181fe205ea9
Wally <1161512420@qq.com>
2022-02-24 00:43:06 +0800

    u

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,18 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+/* Layout */
+import Layout from '@/layout'
+
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '樱盘'
+    }
   },
   {
     path: '/stock',
@@ -41,6 +48,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/redirect',
+    // name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Layout
   }
 ]
 
