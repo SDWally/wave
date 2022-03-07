5d01095d4220b4612283a2a4c7e12a021b70985b
Wally <1161512420@qq.com>
2022-03-08 01:20:53 +0800

    u

--- src/layout/components/navbar.vue
+++ src/layout/components/navbar.vue
@@ -20,7 +20,7 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <!-- <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar"> -->
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -51,7 +51,7 @@ import { mapGetters } from 'vuex'
 // import Hamburger from '@/components/Hamburger'
 // import ErrorLog from '@/components/ErrorLog'
 // import Screenfull from '@/components/Screenfull'
-// import SizeSelect from '@/components/SizeSelect'
+import SizeSelect from '@/components/SizeSelect'
 // import Search from '@/components/HeaderSearch'
 
 export default {
@@ -60,13 +60,13 @@ export default {
     // Hamburger,
     // ErrorLog,
     // Screenfull,
-    // SizeSelect,
+    SizeSelect,
     // Search
   },
   computed: {
     ...mapGetters([
       'sidebar',
-      'avatar',
+      // 'avatar',
       'device'
     ])
   },
--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import router from './router'
 import store from './store'
 import * as echarts from 'echarts'
 import moment  from 'moment'
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
+import enLang from 'element-ui/lib/locale/lang/en'// 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
+import './icons' // icon
+
 Vue.prototype.$echarts = echarts
 Vue.prototype.$moment = moment
 Vue.config.productionTip = false
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  locale: enLang // 如果使用中文，无需设置，请删除
+})
 
 new Vue({
   router,
